b88fb064079d69d17bd7dafc926a3440f671cc74
Etnik <etnik@Etniks-MBP.fritz.box>
2022-04-08 21:58:17 +0200

    fix conflicts

--- newbank/server/NewBankClientHandler.java
+++ newbank/server/NewBankClientHandler.java
@@ -38,31 +38,33 @@ public class NewBankClientHandler extends Thread {
 	public void run() {
 		// keep getting requests from the client and processing them
 		try {
-			// ask for user name
-			out.println("Enter Username");
-			String userName = in.readLine();
-			// ask for password
-			out.println("Enter Password");
-			String password = in.readLine();
-			out.println("Checking Details...");
-			// authenticate user and get customer ID token from bank for use in subsequent requests
-			CustomerID customer = bank.checkLogInDetails(userName, password);
-			// instantiate an user session to tracks login attempts
-			UserSession userS=new UserSession(userName);
-			// if the user is authenticated then get requests from the user and process them
-			if(customer != null) {
-				out.println("Log In Successful. What do you want to do?");
-				while(true) {
-					String request = in.readLine();
-					System.out.println("Request from " + customer.getKey());
-					String response = bank.processRequest(customer, request);
-					out.println(response);
-				}
-			}else{
-				out.println("Log In Failed. Username or password is incorrect!");
-				userS.incrementAttempt();
-				if(userS.getAttempts()>3 && userS.getUsername()==userName){
-					throw new Exception("You tried too many times so you can't log in with that username!");
+			while (true) {
+				// ask for user name
+				out.println("Enter Username");
+				String userName = in.readLine();
+				// ask for password
+				out.println("Enter Password");
+				String password = in.readLine();
+				out.println("Checking Details...");
+				// authenticate user and get customer ID token from bank for use in subsequent requests
+				CustomerID customer = bank.checkLogInDetails(userName, password);
+				// instantiate an user session to tracks login attempts
+				UserSession userS = new UserSession(userName);
+				// if the user is authenticated then get requests from the user and process them
+				if (customer != null) {
+					out.println("Log In Successful. What do you want to do?");
+					while (true) {
+						String request = in.readLine();
+						System.out.println("Request from " + customer.getKey());
+						String response = bank.processRequest(customer, request);
+						out.println(response);
+					}
+				} else {
+					out.println("Log In Failed. Username or password is incorrect!");
+					userS.incrementAttempt();
+					if (userS.getAttempts() > 3 && userS.getUsername() == userName) {
+						throw new Exception("You tried too many times so you can't log in with that username!");
+					}
 				}
 			}
 		} catch (Exception e) {
